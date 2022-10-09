5fba124cae3382e97c18a218f9b33263f5963735
Bartlomiej Winter <bartekwinter3@gmail.com>
2022-10-09 14:49:47 +0100

    Added iterator trait to vec_ptr

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,24 @@ fn main() {
     v.push(3);
     v.insert(1, 99);
     v.update(2, 4);
-    // unsafe {
-    //     println!("UNSAFE 1: {:?}", v);
-    //     println!("UNSAFE 1: {:?}", v.get_val(0));
-    // }
-
+    unsafe {
+        println!("UNSAFE 1: {:?}", v);
+        println!("UNSAFE 1: {:?}", v.get(0));
+    }
     println!("UNSAFE 1: {:?}", v);
     v.swap(0, 1);
     println!("UNSAFE 2: {:?}", v);
     v.remove(0);
     println!("UNSAFE 3: {:?}", v);
+
+    for p in v.iter() {
+        println!("POINTER: {:?}", p);
+    }
     // println!("{:?}", v.get_val(0));
     // println!("{:?}", v.get_val(1));
-
     let vv: Vec<f32> = Vec::new();
 
-    let ptr = v.get(0);
+    //let ptr = v.get(0);
     // unsafe {
     // println!("UNSAFE 2: {:?}", v);
     // println!("UNSAFE 2: {:?}", v.get_val(0));
--- src/vec_ptr.rs
+++ src/vec_ptr.rs
@@ -1,10 +1,13 @@
-use std::{fmt::Debug, ptr};
+use std::{
+    fmt::Debug,
+    ptr,
+    slice::{Iter, IterMut},
+};
 
 #[derive(Debug, Clone, Default)]
 pub struct VecPtr<T> {
     ptrs: Vec<*mut T>,
     raw_vals: Vec<T>,
-    len: usize, // Length of vectors
 }
 
 impl<T: Debug> VecPtr<T> {
@@ -12,33 +15,30 @@ impl<T: Debug> VecPtr<T> {
         VecPtr {
             ptrs: Vec::new(),
             raw_vals: Vec::new(),
-            len: 0,
         }
     }
 
     pub fn push(&mut self, v: T) {
+        //let a: i32 = "str";
         self.raw_vals.push(v);
-        self.len += 1;
-        let ptr: *mut T = &mut self.raw_vals[self.len - 1] as *mut T;
+        let l = self.raw_vals.len();
+        let ptr: *mut T = &mut self.raw_vals[l - 1] as *mut T;
         self.ptrs.push(ptr);
     }
 
     pub fn insert(&mut self, idx: usize, mut v: T) {
         self.ptrs.insert(idx, &mut v as *mut T);
         self.raw_vals.push(v);
-        self.len += 1;
     }
 
     pub fn remove(&mut self, idx: usize) {
         let r_ptr = self.ptrs.remove(idx);
         self.raw_vals.retain(|v| v as *const T != r_ptr);
-        self.len -= 1;
     }
 
     pub fn pop(&mut self) -> Option<*mut T> {
         let ptr = self.ptrs.pop();
         self.raw_vals.pop();
-        self.len -= 1;
         ptr
     }
 
@@ -62,5 +62,41 @@ impl<T: Debug> VecPtr<T> {
 
     pub fn get(&self, idx: usize) -> &T {
         unsafe { &*self.ptrs[idx] }
+    }
+
+    pub fn iter(&self) -> Iter<'_, *mut T> {
+        self.ptrs.iter()
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<'_, *mut T> {
+        self.ptrs.iter_mut()
+    }
+}
+
+pub struct VecPtrIntoIter<T> {
+    ptrs: Vec<*mut T>,
+    idx: usize,
+}
+
+impl<T> IntoIterator for VecPtr<T> {
+    type Item = *mut T;
+
+    type IntoIter = VecPtrIntoIter<T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        VecPtrIntoIter {
+            ptrs: self.ptrs,
+            idx: 0,
+        }
+    }
+}
+
+impl<T> Iterator for VecPtrIntoIter<T> {
+    type Item = *mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let val = self.ptrs[self.idx];
+        self.idx += 1;
+        Some(val)
     }
 }
